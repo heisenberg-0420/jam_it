bf847a890c1ba0e3b4ab2dd104451ef5445fe926
heisenberg-0420 <sasalman00@gmail.com>
2023-12-30 23:05:22 +0500

    setup done

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// export default class App extends Component{
+//   render(){
+//     return <h1>Testing react code</h1>
+//   }
+// }
+
+const App = () => {
+  return <h1>Testing react code</h1>
+};
+
+export default App
